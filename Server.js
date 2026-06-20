9985a1bbe9f1ac722db4b10e320d990391cf81d2
aravindm18092006 <aravindm180906@gmail.com>
2026-06-20 12:06:25 +0530

    Configure Server.js to use process.env.PORT for deployment compatibility

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ dotenv.config({ path: "./Config.env" });
 
 const app = require("./index.js");
 
-const PORT_NO = process.env.PORT_NO;
+const PORT_NO = process.env.PORT || process.env.PORT_NO || 8000;
 
 app.listen(PORT_NO, () => {
   console.log("Server is running on port", PORT_NO);
